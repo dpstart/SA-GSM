45980d4b53811e958eba38d5f55f478881da6f44
Daniele Cappuccio <danielecappuccio@protonmail.com>
2020-01-28 20:33:24 +0100

    class BNInception_GSM_attention created

--- model_zoo/bninception/pytorch_load.py
+++ model_zoo/bninception/pytorch_load.py
@@ -133,6 +133,84 @@ class BNInception_gsm(nn.Module):
                     raise
         return data_dict[self._op_list[-1][2]]
 
+    
+
+class BNInception_GSM_attention(nn.Module):
+    def __init__(self, model_path='model_zoo/bninception/bn_inception_gsm_attention.yaml', num_classes=101,
+                       weight_url='https://yjxiong.blob.core.windows.net/models/bn_inception-9f5701afb96c8044.pth',
+                       num_segments=16, attention_type='general'):
+        super(BNInception_gsm, self).__init__()
+
+        manifest = yaml.load(open(model_path))
+
+        layers = manifest['layers']
+
+        self._channel_dict = dict()
+
+        self._op_list = list()
+        hGates_cnt = 0
+        for l in layers:
+            out_var, op, in_var = parse_expr(l['expr'])
+            if op != 'Concat':
+                if op == 'gsm':
+                    hGates_cnt += 1
+                    id, out_name, module, out_channel, in_name = build_gsm(l,  self._channel_dict[in_var[0]],
+                                                                                 conv_bias=True, num_segments=num_segments)
+
+                    self._channel_dict[out_name] = out_channel
+                    setattr(self, id, module)
+                    self._op_list.append((id, op, out_name, in_name))
+                elif op == 'attention':
+                    hGates_cnt += 1
+                    id, out_name, module, out_channel, in_name = build_attention(l,  self._channel_dict[in_var[0]],
+                                                                                 conv_bias=True, attention_type=attention_type)
+
+                    self._channel_dict[out_name] = out_channel
+                    setattr(self, id, module)
+                    self._op_list.append((id, op, out_name, in_name))
+                else:
+                    id, out_name, module, out_channel, in_name = get_basic_layer(l,
+                                                                    3 if len(self._channel_dict) == 0 else self._channel_dict[in_var[0]],
+                                                                                 conv_bias=True)
+
+                    self._channel_dict[out_name] = out_channel
+                    setattr(self, id, module)
+                    self._op_list.append((id, op, out_name, in_name))
+            else:
+                self._op_list.append((id, op, out_var[0], in_var))
+                channel = sum([self._channel_dict[x] for x in in_var])
+                self._channel_dict[out_var[0]] = channel
+
+        state_dict = torch.utils.model_zoo.load_url(weight_url)
+        for k, v in state_dict.items():
+            state_dict[k] = torch.squeeze(v, dim=0)
+        self.load_state_dict(state_dict, strict=False)
+
+    def forward(self, input):
+        data_dict = dict()
+        data_dict[self._op_list[0][-1]] = input
+
+        def get_hook(name):
+
+            def hook(m, grad_in, grad_out):
+                print(name, grad_out[0].data.abs().mean())
+
+            return hook
+        for op in self._op_list:
+            if op[1] != 'Concat' and op[1] != 'InnerProduct':
+                data_dict[op[2]] = getattr(self, op[0])(data_dict[op[-1]])
+                # getattr(self, op[0]).register_backward_hook(get_hook(op[0]))
+            elif op[1] == 'InnerProduct':
+                x = data_dict[op[-1]]
+                data_dict[op[2]] = getattr(self, op[0])(x.view(x.size(0), -1))
+            else:
+                try:
+                    data_dict[op[2]] = torch.cat(tuple(data_dict[x] for x in op[-1]), 1)
+                except:
+                    for x in op[-1]:
+                        print(x,data_dict[x].size())
+                    raise
+        return data_dict[self._op_list[-1][2]]
 
 class InceptionV3(BNInception):
     def __init__(self, model_path='model_zoo/bninception/inceptionv3.yaml', num_classes=101,
